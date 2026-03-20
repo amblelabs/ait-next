accf07572dce846210a34ade2e94dba602f026ba
Theo <git@theo.is-a.dev>
2026-03-20 22:18:40 +0300

    fix(?): tardis data saving
    Signed-off-by: Theo <git@theo.is-a.dev>

--- common/src/main/java/dev/amble/ait/mixin/tardis/manager/MinecraftServerMixin.java
+++ common/src/main/java/dev/amble/ait/mixin/tardis/manager/MinecraftServerMixin.java
@@ -5,7 +5,10 @@ import dev.amble.ait.api.tardis.TardisManager;
 import dev.amble.ait.common.impl.tardis.ServerTardisManager;
 import net.minecraft.server.MinecraftServer;
 import org.jspecify.annotations.Nullable;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,6 +20,9 @@ import java.util.function.BooleanSupplier;
 @Mixin(MinecraftServer.class)
 public class MinecraftServerMixin implements TardisManager.ManagerLevel<ServerTardis> {
 
+    @Shadow
+    @Final
+    private static Logger LOGGER;
     @Unique
     private @Nullable ServerTardisManager ait$manager;
 
@@ -36,9 +42,11 @@ public class MinecraftServerMixin implements TardisManager.ManagerLevel<ServerTa
             this.ait$manager.tick();
     }
 
-    @Inject(method = "saveEverything", at = @At("TAIL"))
+    @Inject(method = "saveAllChunks", at = @At("TAIL"))
     public void save(boolean suppressLog, boolean flush, boolean forced, CallbackInfoReturnable<Boolean> cir) {
-        if (this.ait$manager != null)
+        if (this.ait$manager != null) {
             this.ait$manager.save();
+            LOGGER.info("ServerTardisManager: All TARDIS' are saved");
+        }
     }
 }
